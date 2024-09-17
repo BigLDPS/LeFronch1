dec69641b28b5b8e6a4c46d81bcaa619c0c8a03c
L. Price <lpric29@lsu.edu>
2024-09-16 19:04:15 -0500

    Add.java added, adds two numbers based on scanners.

---
+++ add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+
+class add {
+  public static void main(String[] args) {
+    int x, y;
+    int sum;
+    Scanner scanner = new Scanner(System.in); 
+    System.out.println("First number to add:");
+    x = scanner.nextInt(); 
+
+    System.out.println("Second number to add:");
+    y = scanner.nextInt(); 
+
+    sum = x + y; 
+    System.out.println("Sum is: " + sum);
+  }
+} 
